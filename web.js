e794ba83e6ec81daae3fc6f6306d11f9e150617b
Slava Belokurski <slavchoo2@gmail.com>
2014-06-06 02:58:17 +0300

    build

--- web.js
+++ web.js
@@ -5,10 +5,6 @@ var app = express();
 app.use(express.logger('dev'));
 app.use(gzippo.staticGzip("" + __dirname + "/dist"));
 
-app.get('/*', function(req, res) {
-    res.sendfile(__dirname+'/dist/index.html');
-});
-
 app.listen(process.env.PORT || 5000, function() {
     console.log('start listening');
 });
