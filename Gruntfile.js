691780e80d5acc3f89ba971849136e7c21681cab
Slava Belokurski <slavchoo2@gmail.com>
2014-06-06 02:16:45 +0300

    build

--- Gruntfile.js
+++ Gruntfile.js
@@ -161,6 +161,7 @@ module.exports = function (grunt) {
           '<%= yeoman.dist %>/scripts/{,*/}*.js',
           '<%= yeoman.dist %>/styles/{,*/}*.css',
           '<%= yeoman.dist %>/images/{,*/}*.{png,jpg,jpeg,gif,webp,svg}',
+          '!<%= yeoman.dist %>/images/src/{,*/}*.{png,jpg,jpeg,gif,webp,svg}',
           '<%= yeoman.dist %>/styles/fonts/*'
         ]
       }
