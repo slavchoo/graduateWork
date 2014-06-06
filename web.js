212bef7f76451343a6f34ab756d0e57d368b2a18
Slava Belokurski <slavchoo2@gmail.com>
2014-06-06 03:06:23 +0300

    build

--- web.js
+++ web.js
@@ -1,9 +1,8 @@
 var gzippo = require('gzippo');
 var express = require('express');
-var morgan  = require('morgan');
 var app = express();
 
-app.use(morgan());
+app.use(express.logger('dev'));
 app.use(gzippo.staticGzip("" + __dirname + "/dist"));
 
 app.listen(process.env.PORT || 5000, function() {
